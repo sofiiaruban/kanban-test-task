8cc405d1a5eeed3487c813b6d82367e3eeb4ae8a
sofiiaruban <sophiaruban249324@gmail.com>
2025-03-08 21:12:12 +0200

    feat: Add routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from './components/ui/provider.tsx'
 import App from './App.tsx'
-import { Provider } from "@/components/ui/provider"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import { Box } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Box
+      p="4"
+      borderWidth="1px"
+      borderColor="border.disabled"
+      color="fg.disabled"
+    >
+      Somewhat disabled box
+    </Box>
+  )
+}
+
+export default Home
